2ada051942363ad14d9a5fde6e63d183d2c0c95f
spacey-g <thissgrace@gmail.com>
2025-12-15 20:06:28 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,11 @@
+import pandas as pd
+
+def save_df(df, path):
+    """Save DataFrame as CSV in the data folder."""
+    df.to_csv(path, index=False)
+    print(f"Saved: {path}")
+
+def preview_df(df, n=5):
+    """Preview top rows of DataFrame."""
+    print(df.head(n))
 
